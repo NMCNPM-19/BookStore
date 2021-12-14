6ceca0ab65713009e65a7a689342134b32359694
tienak1 <thattien1111@gmail.com>
2021-12-14 20:59:14 +0700

    route check

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,10 +6,18 @@ const RouterProduct = require('./Products')
 function route(app){
     
     app.use('/',RouterSite)
-    app.use('/products',RouterProduct)
-    app.use('/user',RouterProfile)
-    app.use('/accounts',RouterAccount)
-    app.use('/customers',RouterCustomer)
+    app.use((req, res, next) => {
+        if(req.user) {
+            app.use('/products',RouterProduct)
+            app.use('/user',RouterProfile)
+            app.use('/accounts',RouterAccount)
+            app.use('/customers',RouterCustomer)
+        }
+        else {
+            res.redirect('/');
+        }
+        next();
+    });
 }
 
 module.exports = route 
