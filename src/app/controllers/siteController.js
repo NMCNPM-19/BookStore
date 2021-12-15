8f98b5b69f75b24d90b300bb0d4cc8f903d9746c
shinobihao2001 <shinobihao2001@gmail.com>
2021-12-15 13:51:31 +0700

    add dashbroad

--- src/app/controllers/siteController.js
+++ src/app/controllers/siteController.js
@@ -1,14 +1,38 @@
+const pagination = require('../../public/js/pages/pagination');
+const productService = require('../services/productService');
+const {multipleSequelizeToObject,SequelizeToObject} = require('../../util/sequelize');
+const e = require('express');
 
 
 class SiteController{
-    index(req, res, next){
-        //res.render('login', { layout: false, wrongLogin: req.query.wrongLogin !== undefined} );
+    async index(req, res, next){
         if (!req.user) {
             res.redirect('/login');
         }
         else {
-            res.render('index', {user: req.user});
+            if(!req.user.emp) {
+                const itemPerPage = 10;
+                const page = !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0;
+                const title = req.query.title
+                const products = await productService.list(title,page,itemPerPage);
+                const TotalPage = Math.ceil(products.count/itemPerPage) > page + 1 ? Math.ceil(products.count/itemPerPage) : page + 1
+                const pagItems = pagination.paginationFunc( +1, TotalPage);
+
+                
+                console.log(title)
+
+
+                res.render('index', {
+                    Items: pagItems,
+                    products: products.rows,
+                    title: title,
+                });
+            } else {
+            res.redirect('/');
+            }
         }
     }
 }
+
+
 module.exports = new SiteController;
