a42a344bae759c014663e99eef221dab7b337f56
shinobihao2001 <shinobihao2001@gmail.com>
2021-12-15 16:54:49 +0700

    add storage

--- src/app/controllers/storageController.js
+++ src/app/controllers/storageController.js
@@ -1,5 +1,5 @@
 const pagination = require('../../public/js/pages/pagination');
-const productService = require('../services/storageService');
+const storageService = require('../services/storageService');
 const {multipleSequelizeToObject,SequelizeToObject} = require('../../util/sequelize');
 const e = require('express');
 
@@ -7,12 +7,39 @@ const e = require('express');
 class storageController{
     //[GET]:\storage
     async list(req, res, next){
-        console.log('hhh');
-        if(req.user){
-            res.render('storage')
-        } else{
-            res.redirect('/');
-        }
+     //   if(req.user){
+            const itemPerPage = 10;
+            const title = req.query.title;
+            let chooseMonth=req.query.chooseMonth;
+            let secondChooseMonth=chooseMonth;
+            var month;
+            if (chooseMonth){
+                month=chooseMonth.split("-");
+                month=month.join('');
+            }
+            else {
+                let date=new Date;
+                month=date.getFullYear().toString()+"-"+(date.getMonth()+1).toString();
+                console.log(date.getMonth());
+                console.log(month);
+                secondChooseMonth=month;
+            }
+                
+            console.log(month);
+            const page = !isNaN(req.query.page) && req.query.page > 0 ? req.query.page - 1 : 0;
+            const storageBook = await storageService.list(title,month,page,itemPerPage)
+            const TotalPage = Math.ceil(storageBook.count/itemPerPage) > page + 1 ? Math.ceil(storageBook.count/itemPerPage) : page + 1
+            const pagItems = pagination.paginationFunc(page+1, TotalPage);
+            res.render('storage',{
+                Items: pagItems,
+                storageBook: storageBook.rows,
+                message: req.query.message,
+                title: title,
+                chooseMonth: secondChooseMonth,
+            })
+       // } else{
+      //      res.redirect('/');
+      //  }
     }
 }
 
