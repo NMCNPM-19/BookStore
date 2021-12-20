cc38f5ec0b04275bd5494f4ef407c66a01cea2be
shinobihao2001 <shinobihao2001@gmail.com>
2021-12-20 19:41:11 +0700

    fix rule form

--- src/app/services/ruleService.js
+++ src/app/services/ruleService.js
@@ -11,14 +11,19 @@ exports.getRule=()=>{
 
 exports.updateSave = async (req) => {
     const Rules = await models.rules.findOne();
-
+    console.log("Body trước");
+    console.log(req.body);
     req.body.import_min=Number(req.body.import_min);
     req.body.curr_quantity_max=Number(req.body.curr_quantity_max);
     req.body.dept_min=Number(req.body.dept_min);
-    req.body.dept_min=Number(req.body.dept_min);
     req.body.curr_sale_min=Number(req.body.curr_sale_min);
-    req.total_cost_exceed_debt=Number(req.body.total_cost_exceed_debt);
-    Rules.set(req.body)
+    if (req.body.total_cost_exceed_debt){
+        req.body.total_cost_exceed_debt=Number(req.body.total_cost_exceed_debt)
+    }
+    else{
+        req.body.total_cost_exceed_debt=Number(0)
+    }
+    Rules.set(req.body);
     console.log(req.body);
     await Rules.save()
 }
